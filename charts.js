e7c61024fb447812ce8cb830ab35aec728c3d402
saunders77 <michaelsaunders@outlook.com>
2016-08-24 15:28:40 -0700

    lines

--- charts.js
+++ charts.js
@@ -5,6 +5,8 @@ var chartNames = [];
 var chartRanges = [];
 var seriesNames = [];
 
+console.log("hello");
+
 function playChart(){
     var myChartIndex = parseInt(document.getElementById("selectItem").value);
     console.log("series box value is: " + document.getElementById("selectSeries").value);
@@ -165,7 +167,7 @@ function refreshCharts(){
     });
 }
 
-(function () {
+//(function () {
     Office.initialize = function (reason) {
 
         if(getParameterByName('action') == 'charts'){
@@ -182,9 +184,9 @@ function refreshCharts(){
         }
         
     };
-})();
+//})();
 
-
+console.log("goodbye");
 //Notice function needs to be in global namespace
 /*
 function insertAStock(event){
